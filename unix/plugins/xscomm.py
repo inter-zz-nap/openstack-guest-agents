34d84bb151d6cc1013d15cf23cb62c9962fdd6aa
Chris Behrens <cbehrens@codestud.com>
2011-04-18 12:50:33 -0700

    Fix LP:764221 (mkdir the request path before trying to get entries from it) Allow request/response paths to be passed as init args to XSComm add a bunch of nasty exception checking to make sure we end transactions on exceptions as well as reset the xs_handle on errors

--- unix/plugins/xscomm.py
+++ unix/plugins/xscomm.py
@@ -48,26 +48,111 @@ class XSComm(object):
     XenStore communication plugin for nova-agent
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+
+        self.request_path = kwargs.get("request_path",
+                XENSTORE_REQUEST_PATH)
+        self.response_path = kwargs.get("response_path",
+                XENSTORE_RESPONSE_PATH)
+
         self.xs_handle = pyxenstore.Handle()
+        self.xs_handle.mkdir(self.request_path)
         self.requests = []
+
+    def _check_handle(self):
+        if not self.xs_handle:
+            self.xs_handle = pyxenstore.Handle()
 
     def _get_requests(self):
         """
         Get requests out of XenStore and cache for later use
         """
 
-        self.xs_handle.transaction_start()
+        self._check_handle()
 
-        entries = self.xs_handle.entries(XENSTORE_REQUEST_PATH)
+        try:
+            self.xs_handle.transaction_start()
+        except pyxenstore.PyXenStoreError, e:
+            # Need to have the handle reopened later
+            self.xs_handle = None
+            raise e
+
+        try:
+            entries = self.xs_handle.entries(self.request_path)
+        except pyxenstore.NotFoundError:
+            # Someone removed the path on us ?
+            try:
+                self.xs_handle.transaction_end()
+            except Exception:
+                # No matter what exception we get, since we couldn't
+                # end the transaction, we're going to need to reopen
+                # the handle later
+                self.xs_handle = None
+
+            try:
+                self.xs_handle.mkdir(self.request_path)
+            except pyxenstore.PyXenStoreError, e:
+                # Need to have the handle reopened later
+                self.xs_handle = None
+                raise e
+            # Re-try after mkdir()
+            return self._get_requests()
+        except pyxenstore.PyXenStoreError, e:
+            # Any other XenStore errors need to have handle reopened
+            self.xs_handle = None
+            raise e
+        except Exception, e:
+            try:
+                self.xs_handle.transaction_end()
+            except:
+                # No matter what exception we get, since we couldn't
+                # end the transaction, we're going to need to reopen
+                # the handle later
+                self.xs_handle = None
+                raise e
+            raise e
 
         for entry in entries:
-            path = XENSTORE_REQUEST_PATH + '/' + entry
-            data = self.xs_handle.read(path)
+            path = self.request_path + '/' + entry
+            try:
+                data = self.xs_handle.read(path)
+            except pyxenstore.NotFoundError:
+                continue
+            except pyxenstore.PyXenStoreError, e:
+                # Any other XenStore errors need to have handle reopened
+                self.xs_handle = None
+                raise e
+            except Exception, e:
+                try:
+                    self.xs_handle.transaction_end()
+                except:
+                    # No matter what exception we get, since we couldn't
+                    # end the transaction, we're going to need to reopen
+                    # the handle later
+                    self.xs_handle = None
+                raise e
 
-            self.requests.append({'path': path, 'data': data})
+            try:
+                self.requests.append({'path': path, 'data': data})
+            except Exception, e:
+                try:
+                    self.xs_handle.transaction_end()
+                except:
+                    # No matter what exception we get, since we couldn't
+                    # end the transaction, we're going to need to reopen
+                    # the handle later
+                    self.xs_handle = None
+                raise e
 
-        self.xs_handle.transaction_end()
+        try:
+            self.xs_handle.transaction_end()
+        except:
+            # No matter what exception we get, since we couldn't
+            # end the transaction, we're going to need to reopen
+            # the handle later
+            self.xs_handle = None
+            raise e
+        return len(self.requests) > 0
 
     def get_request(self):
         """
@@ -86,9 +171,20 @@ class XSComm(object):
         Remove original request from XenStore and write out the response
         """
 
-        self.xs_handle.rm(req['path'])
+        self._check_handle()
+
+        try:
+            self.xs_handle.rm(req['path'])
+        except pyxenstore.PyXenStoreError, e:
+            self.xs_handle = None
+            self._check_handle()
+            # Fall through...
 
         basename = req['path'].rsplit('/', 1)[1]
-        resp_path = XENSTORE_RESPONSE_PATH + '/' + basename
+        resp_path = self.response_path + '/' + basename
 
-        self.xs_handle.write(resp_path, resp['data'])
+        try:
+            self.xs_handle.write(resp_path, resp['data'])
+        except pyxenstore.PyXenStoreError, e:
+            self.xs_handle = None
+            raise e
